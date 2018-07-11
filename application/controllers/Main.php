61375027469084ccc8e898d19a42075ba3e619bb
Munch2018 <doublenill5@naver.com>
2018-07-11 20:40:52 +0900

    먼치하기 링크달기

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -10,6 +10,8 @@ class Main extends CI_Controller
 
     public function index()
     {
+        $this->load->view('common/header');
         $this->load->view('Main/index');
+        $this->load->view('common/footer');
     }
 }
