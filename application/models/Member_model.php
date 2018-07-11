b03cf0a3498744dea14ab7cde7622056cf1dc979
kimeunju <eunjju1209@gmail.com>
2018-07-11 23:54:55 +0900

    로그인 기능 붙히기

---
+++ application/models/Member_model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eunju
+ * Date: 2018-07-11
+ * Time: 오후 11:35
+ */
+
+public class Member_model extends CI_Model{
+
+    public function __construct(){
+        parent::__construct();
+    }
+}
