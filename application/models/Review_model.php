93521386f08bc03edd2c0a539c630ae4521716d8
Munch2018 <doublenill5@naver.com>
2018-08-12 16:56:55 +0900

    admin review 작업

--- application/models/Review_model.php
+++ application/models/Review_model.php
@@ -13,6 +13,19 @@ class Review_model extends CI_Model{
         parent::__construct();
     }
 
+    private function setWhere($where = [])
+    {
+        if (isset($where['where']) && !empty($where['where'])) {
+            foreach ($where['where'] as $key => $value) {
+                $this->db->where($key, $value);
+            }
+        }
+
+        if (isset($where['offset']) && isset($where['limit'])) {
+            $this->db->limit($where['limit'] , $where['offset']);
+        }
+    }
+
     /**
      * 리뷰 등록하는 모델
      */
@@ -36,5 +49,135 @@ class Review_model extends CI_Model{
         } else {
             return false;
         }
+    }
+    
+    /**
+     * 리뷰리스트 가져오기
+     */
+
+    public function getList($where = [])
+    {
+
+        if (!empty($where)) {
+            $this->setWhere($where);
+        }
+
+//        return $this->db->get('review')->result_array();
+        $return = $this->db->get('review')->result_array();
+        echo '<pre>';
+        print_r($where);
+        print_r($this->db->last_query());
+        echo '</pre>';
+        return $return;
+    }
+
+    public function getCount($where = [])
+    {
+        if (!empty($where)) {
+            if (isset($where['offset'])) {
+                unset($where['offset']);
+            }
+
+            if (isset($limit)) {
+                unset($where['limit']);
+            }
+
+            $this->setWhere($where);
+        }
+
+        return $this->db->count_all_results('review');
+//        $return = $this->db->count_all_results('review');
+//
+//        echo '<pre>';
+//        print_r($this->db->last_query());
+//        echo '</pre>';
+//        return $return;
+
+    }
+
+    /*
+     * review 하나만 가져오기
+     * */
+    public function getReview($where = [])
+    {   
+        if (!empty($where)) {
+            $this->setWhere($where);
+        }
+
+//        $return = $this->db->get('review')->row_array();
+//        echo '<pre>';
+//        print_r($this->db->last_query());
+//        echo '</pre>';
+//        return $return;
+
+        return $this->db->get('review')->row_array();
+    }
+
+    /**
+     * review update처리해주기
+     * 첫 parameter => set 할 데이터들 넣어준다.
+     * 두번ㅉㅐ array 에서는 where 조건들 넣어준다.
+     * return boolean
+     */
+
+    public function doUpdate($data = [] , $where = [])
+    {
+        if (!empty($where)) {
+
+            $this->setWhere($where);
+
+            $this->db->trans_begin();
+
+            $this->db->update('review', $data);
+
+            if ($this->db->trans_status() === false) {
+                $this->db->trans_rollback();
+                return false;
+            } else {
+                $this->db->trans_commit();
+                echo '<pre>';
+                print_r($this->db->last_query());
+                echo '</pre>';
+                return true;
+            }
+        } else {
+            return false;
+        }
+        return false;
+    }
+
+
+    /**
+     * review delete 처리
+     * 두번ㅉㅐ array 에서는 where 조건들 넣어준다.
+     * return boolean
+     */
+
+    public function doDelete($where = [])
+    {
+        if (!empty($where)) {
+            $this->setWhere($where);
+
+            $data['use_fl'] = 'n';
+
+            // edit_dt & delete_dt 다 현재 시간으로 update 해주고
+            // edid_idx && delete_idx 세션으로 어드민 아이디로 처리해주기
+
+            $data['edit_dt'] = date("Y-m-d H:i:s");
+            $data['del_dt'] = date("Y-m-d H:i:s");
+
+            $data['edit_idx'] = 1;
+            $data['del_idx'] = 1;
+
+            $this->db->update('review', $data);
+
+            return true;
+
+        }else {
+            return false;
+        }
+
+        return false;
+
     }
 }
