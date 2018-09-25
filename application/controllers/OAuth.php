a4f122a7bbf406f73cc4f21c0364042dd3d721c9
Munch2018 <doublenill5@naver.com>
2018-09-26 01:23:32 +0900

    0926

--- application/controllers/OAuth.php
+++ application/controllers/OAuth.php
@@ -17,9 +17,7 @@ class OAuth extends CI_Controller
 
     public function index()
     {
-        echo print_r($_POST, 1);
-        echo print_r($_GET, 1);
-        exit;
+
     }
 
 
@@ -44,26 +42,42 @@ class OAuth extends CI_Controller
         }
     }
 
+    public function naver()
+    {
+        echo print_r($_POST, 1);
+        echo print_r($_GET, 1);
+        exit;
+    }
+
     public function naverLogin()
     {
         $client_id = "kdCLf_xSwQuoDQPhffGy"; // 위에서 발급받은 Client ID 입력
-        $redirectURI = urlencode("http://munchmunch.kr/OAuth"); //자신의 Callback URL 입력
+        $redirectURI = urlencode("http://munchmunch.kr/OAuth/naver"); //자신의 Callback URL 입력
         $state = "RAMDOM_STATE";
-        $apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=".$client_id."&redirect_uri=".$redirectURI."&state=".$state;
+        $apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" . $client_id . "&redirect_uri=" . $redirectURI . "&state=" . $state;
 
         redirect($apiURL);
     }
 
     public function facebookLogin()
     {
+        echo print_r($_POST, 1);
+        echo print_r($_GET, 1);
+        exit;
+    }
 
+    public function kakao()
+    {
+        echo print_r($_POST, 1);
+        echo print_r($_GET, 1);
+        exit;
     }
 
     public function kakaoLogin()
     {
         $restAPIKey = "10937aa222a35af6980c19eb574b9def"; //본인의 REST API KEY를 입력해주세요
-        $callbacURI = urlencode("http://munchmunch.kr/OAuth"); //본인의 Call Back URL을 입력해주세요
-        $kakaoLoginUrl = "https://kauth.kakao.com/oauth/authorize?client_id=".$restAPIKey."&redirect_uri=".$callbacURI."&response_type=code";
+        $callbacURI = urlencode("http://munchmunch.kr/OAuth/kakao"); //본인의 Call Back URL을 입력해주세요
+        $kakaoLoginUrl = "https://kauth.kakao.com/oauth/authorize?client_id=" . $restAPIKey . "&redirect_uri=" . $callbacURI . "&response_type=code";
 
         redirect($kakaoLoginUrl);
     }
