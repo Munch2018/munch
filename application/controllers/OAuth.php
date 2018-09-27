f77ca63a0590efe4c9e37a17259460fb2d9b472a
Munch2018 <doublenill5@naver.com>
2018-09-27 22:27:46 +0900

    0927

--- application/controllers/OAuth.php
+++ application/controllers/OAuth.php
@@ -11,6 +11,12 @@ class OAuth extends CI_Controller
 {
     const KAKAO_CLIENT_ID = '10937aa222a35af6980c19eb574b9def';
     const KAKAO_CLIENT_RETURN = "http://munchmunch.kr/OAuth/kakao";
+    const KAKAO_CLIENT_ME_RETURN = "https://kapi.kakao.com/v2/user/me";
+
+    const NAVER_CLIENT_ID = "kdCLf_xSwQuoDQPhffGy";
+    const NEVER_CLIENT_SECRET = "CcAS1FT7Sr";
+    const NAVER_CLIENT_RETURN = "http://munchmunch.kr/OAuth/naver";
+    const NAVER_CLIENT_ME_RETURN = "https://openapi.naver.com/v1/nid/me";
 
 
     public function __construct()
@@ -43,13 +49,103 @@ class OAuth extends CI_Controller
 
     public function naver()
     {
+        $client_id = self::NAVER_CLIENT_ID;   //ClientID 입력
+        $client_secret = self::NEVER_CLIENT_SECRET; //Client Secret 입력
+
+        $code = $_GET["code"];
+        $state = $_GET["state"];
+        $redirectURI = urlencode(self::NAVER_CLIENT_RETURN); // 현재 Callback Url 입력
+
+        $url = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=".$client_id."&client_secret=".$client_secret."&redirect_uri=".$redirectURI."&code=".$code."&state=".$state;
+        $is_post = false;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, $is_post);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $headers = array();
+        $response = curl_exec ($ch);
+        $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        echo "status_code:".$status_code;
+
+        curl_close($ch);
+
+        if ($status_code !== 200) {
+            alert('로그인에 실패하였습니다 ', '', 1);
+            return false;
+        }
+
+        $responseArr = json_decode($response, true);
+        $_SESSION['naver_access_token'] = $responseArr['access_token'];
+        $_SESSION['naver_refresh_token'] = $responseArr['refresh_token'];
+        $_SESSION['naver_refresh_token_expires_in'] = $responseArr['expires_in'];
+
+        // 토큰값으로 네이버 회원정보 가져오기
+        $me_headers = array(
+            'Content-Type: application/json',
+            sprintf('Authorization: Bearer %s', $responseArr['access_token'])
+        );
+
+        $me_is_post = false;
+        $me_ch = curl_init();
+        curl_setopt($me_ch, CURLOPT_URL, self::NAVER_CLIENT_ME_RETURN);
+        curl_setopt($me_ch, CURLOPT_POST, $me_is_post);
+        curl_setopt($me_ch, CURLOPT_HTTPHEADER, $me_headers);
+        curl_setopt($me_ch, CURLOPT_RETURNTRANSFER, true);
+        $me_response = curl_exec ($me_ch);
+        $me_status_code = curl_getinfo($me_ch, CURLINFO_HTTP_CODE);
+        curl_close ($me_ch);
+
+        $me_responseArr = json_decode($me_response, true);
+echo print_r($me_responseArr,1);
+exit;
+        if (!empty($responseArr['response']['email'])) {
+            $email = $responseArr['response']['email'];
+
+            $alreadyData = $this->auth_model->getMemberSns([
+                'type' => 'naver',
+                'email' => $email,
+                'token' => $responseArr['access_token'],
+            ]);
+
+            //회원정보가 있다면
+            if (!empty($alreadyData['member_sns_idx'])) {
+                if ($this->auth_model->updateToken([
+                    'token' => $responseArr['access_token'],
+                    'refresh_token' => $responseArr['refresh_token'],
+                    'member_sns_idx' => $alreadyData['member_sns_idx']
+                ])) {
+                    $this->login($alreadyData);
+                } else {
+                    alert('로그인에 실패하였습니다.', '', 1);
+                    return false;
+                }
+            } else {
+                // properties 항목은 카카오 회원이 설정한 경우만 넘겨 받습니다.
+                $email = $me_responseArr['response']['email']; // 이메일
+                $name = $me_responseArr['response']['nickname']; // 닉네임
+
+                // 멤버 DB에 토큰과 회원정보를 넣고 로그인
+                if (!$this->join([
+                    'type' => 'naver',
+                    'email' => $email,
+                    'name' => !empty($name) ? $name : 'naver',
+                    'token' => $responseArr['access_token'],
+                    'refresh_token' => $responseArr['refresh_token'],
+                ])) {
+                    alert('로그인에 실패하였습니다.', '', 1);
+                    return false;
+                }
+            }
+        }
 
     }
 
     public function naverLogin()
     {
-        $client_id = "kdCLf_xSwQuoDQPhffGy"; // 위에서 발급받은 Client ID 입력
-        $redirectURI = urlencode("http://munchmunch.kr/OAuth/naver"); //자신의 Callback URL 입력
+        $client_id = self::NAVER_CLIENT_ID; // 위에서 발급받은 Client ID 입력
+        $redirectURI = urlencode(self::NAVER_CLIENT_RETURN); //자신의 Callback URL 입력
         $state = "RAMDOM_STATE";
         $apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" . $client_id . "&redirect_uri=" . $redirectURI . "&state=" . $state;
 
@@ -89,7 +185,7 @@ class OAuth extends CI_Controller
         $_SESSION['kakao_refresh_token_expires_in'] = $responseArr['refresh_token_expires_in'];
 
         //사용자 정보 가저오기
-        $USER_API_URL = "https://kapi.kakao.com/v2/user/me";
+        $USER_API_URL = self::KAKAO_CLIENT_ME_RETURN;
         $opts = array(
             CURLOPT_URL => $USER_API_URL,
             CURLOPT_SSL_VERIFYPEER => false,
@@ -115,13 +211,14 @@ class OAuth extends CI_Controller
             $alreadyData = $this->auth_model->getMemberSns([
                 'type' => 'kakao',
                 'email' => $email,
-                'token' => $responseArr['access_token']
+                'token' => $responseArr['access_token'],
             ]);
 
             //회원정보가 있다면
             if (!empty($alreadyData['member_sns_idx'])) {
                 if ($this->auth_model->updateToken([
-                    'token' => $responseArr['refresh_token'],
+                    'token' => $responseArr['access_token'],
+                    'refresh_token' => $responseArr['refresh_token'],
                     'member_sns_idx' => $alreadyData['member_sns_idx']
                 ])) {
                     $this->login($alreadyData);
@@ -139,7 +236,8 @@ class OAuth extends CI_Controller
                     'type' => 'kakao',
                     'email' => $email,
                     'name' => !empty($name) ? $name : 'kakao',
-                    'token' => $responseArr['access_token']
+                    'token' => $responseArr['access_token'],
+                    'refresh_token' => $responseArr['refresh_token'],
                 ])) {
                     alert('로그인에 실패하였습니다.', '', 1);
                     return false;
