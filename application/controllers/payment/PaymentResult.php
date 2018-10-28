c9f6aae1b7cdaa8f9f5a3b77b895fe4b8d8a3510
Munch2018 <doublenill5@naver.com>
2018-10-28 22:22:32 +0900

    10.28

---
+++ application/controllers/payment/PaymentResult.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jungmin
+ * Date: 2018-10-28
+ * Time: 오후 10:20
+ */
+
+class PaymentResult  extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+
+    }
+}
